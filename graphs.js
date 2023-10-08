c98a5826cc9f07562cd0c49820bf24901c87e265
shuenyng <110533877+shuenyng@users.noreply.github.com>
2023-10-08 22:07:40 +1100

    Add files via upload

--- graphs.js
+++ graphs.js
@@ -8,7 +8,7 @@ var spec2 = "uv_index_map.json";
     // Access the Vega view instance for co_region chart
   }).catch(console.error);
 
-var spec3 = "humidity_region_country.json"; // Specify the URL or file path for your humidity chart specification
-  vegaEmbed('#humidity_region_country', spec3).then(function(result) {
+  var spec3 = "temp.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#temp', spec3).then(function(result) {
     // Access the Vega view instance for the humidity chart
   }).catch(console.error);
