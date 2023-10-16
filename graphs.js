4ea252e5bc41fab50158339ae765ae1b9536e2ae
shuenyng <110533877+shuenyng@users.noreply.github.com>
2023-10-17 04:38:25 +1100

    Update graphs.js

--- graphs.js
+++ graphs.js
@@ -1,33 +1,57 @@
-var spec1 = "air_quality_map.json"; 
-  vegaEmbed('#air_quality_map', spec2).then(function(result) {
+var spec = "life_expectancy.vega.json"; 
+  vegaEmbed('#life_expectancy', spec).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-var spec2 = "uv_index_map.json"; 
-  vegaEmbed('#uv_index_map', spec2).then(function(result) {
+var spec1 = "infant_mortality.vega.json"; 
+  vegaEmbed('#infant_mortality', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
-  var spec3 = "co_region.json"; 
-  vegaEmbed('#co_region', spec3).then(function(result) {
+  var spec2 = "fertility_rate.vega.json"; 
+  vegaEmbed('#fertility_rate', spec2).then(function(result) {
     // Access the Vega view instance for co_region chart
   }).catch(console.error);
 
-  var spec4 = "humidity.vega.json"; // Specify the URL or file path for your humidity chart specification
-  vegaEmbed('#humidity', spec4).then(function(result) {
+  var spec3 = "unemployment_rate.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#unemployment_rate', spec3).then(function(result) {
     // Access the Vega view instance for the humidity chart
   }).catch(console.error);
 
-  var spec5 = "co_temp.vega.json"; // Specify the URL or file path for your humidity chart specification
-  vegaEmbed('#co_temp', spec5).then(function(result) {
+  var spec4 = "maternal_mortality.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#maternal_mortality', spec4).then(function(result) {
     // Access the Vega view instance for the co_temp chart
   }).catch(console.error);
 
-  var spec6 = "heatmap.vega.json"; // Specify the URL or file path for your humidity chart specification
-  vegaEmbed('#heatmap', spec6).then(function(result) {
+  var spec5 = "minimum_wage.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#minimum_wage', spec5).then(function(result) {
     // Access the Vega view instance for the heatmap chart
   }).catch(console.error);
 
+  var spec6 = "gasoline_prices.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#gasoline_prices', spec6).then(function(result) {
+    // Access the Vega view instance for the heatmap chart
+  }).catch(console.error);
+
+  var spec7 = "population.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#population', spec7).then(function(result) {
+    // Access the Vega view instance for the heatmap chart
+  }).catch(console.error);
+
+  var spec8 = "stacked.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#stacked', spec8).then(function(result) {
+    // Access the Vega view instance for the heatmap chart
+  }).catch(console.error);
+
+  var spec9 = "birth_fertility.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#birth_fertility', spec9).then(function(result) {
+    // Access the Vega view instance for the heatmap chart
+  }).catch(console.error);
+
+  var spec10 = "infant_life.vega.json"; // Specify the URL or file path for your humidity chart specification
+  vegaEmbed('#infant_life', spec10).then(function(result) {
+    // Access the Vega view instance for the heatmap chart
+  }).catch(console.error);
 
 
   
